d8bf65577efcaf580f9f6375f7b75b245f8a1a87
Radek Szyprowski <radek.szyper@gmail.com>
2022-01-11 02:15:32 +0100

    HelloWorld!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {                           // [1]
+    public static void main(String[] args) {         // [2]
+        System.out.println("hello world"); // [3]
+        System.out.println("radek");
+        System.out.println("40");
+        System.out.println("sz");
+    }
+
+}
