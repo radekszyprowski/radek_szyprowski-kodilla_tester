948dfdb04d0af7bc2d0c4dccf1021dd3f7fa9c7c
Radek Szyprowski <radek.szyper@gmail.com>
2022-01-14 00:53:04 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,11 @@
+public class MainExtended  {
+    public static void main (String[] args) {
+        String myNick = "radek.sz";
+        String myQuote = "Sad but true";
+        int myWeight = 65;
+        double myHeight = 1.76;
+        char drivingLicense = 'B';
+        boolean isSafeDriver = true;
+        System.out.println (myQuote);
+    }
+}
