409d9aa648a3ccccf71b7067c5ff56d611f389bc
Radek Szyprowski <radek.szyper@gmail.com>
2022-01-14 20:32:20 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Maciej";
+        String example = "Hello from FirstClass!";
+        int myAge = 22;
+        double height = 1.78;
+        char carClass = 'A';
+        boolean isGoodProgrammer = true;
+        System.out.println (example);
+    }
+}
