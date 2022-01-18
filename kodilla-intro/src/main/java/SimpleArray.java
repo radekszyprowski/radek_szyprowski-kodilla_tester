1d0e21a3a8c15c3fd51d71afbc36888d7e446903
Radek Szyprowski <radek.szyper@gmail.com>
2022-01-18 01:52:21 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,12 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] movies = new String[5];
+        movies[0] = "Blade Runner";
+        movies[1] = "Pulp Fiction";
+        movies[2] = "Nobody";
+        movies[3] = "El Camino";
+        movies[4] = "Sara";
+        String movie = movies[3];
+        System.out.println (movie);
+    }
+}
